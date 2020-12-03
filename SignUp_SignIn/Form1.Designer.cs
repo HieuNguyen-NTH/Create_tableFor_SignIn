f68d782307ce47cffa9d429548561933a532f7fb
HieuNguyen-NTH <66076345+HieuNguyen-NTH@users.noreply.github.com>
2020-12-03 23:05:25 +0700

    first commit

---
+++ SignUp_SignIn/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SignUp_SignIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign Up";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Email";
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(228, 150);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(277, 26);
+            this.textBox_username.TabIndex = 6;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(228, 205);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(277, 26);
+            this.textBox_password.TabIndex = 7;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(228, 261);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(277, 26);
+            this.textBox_email.TabIndex = 8;
+            this.textBox_email.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_email_KeyDown);
+            this.textBox_email.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_email_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.TextBox textBox_email;
+    }
+}
+
---
+++ SignUp_SignIn/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignUp_SignIn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool Format_email( string a )
+        {
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //kiểm tra định dạng gmail
+            string s = textBox_email.Text.ToString();
+            bool exist_signal = false;
+            for(int i=0;i<s.Length;i++)
+            {
+                if (s[i] == '@')
+                    exist_signal = true;
+            }
+            if (exist_signal == false)
+                MessageBox.Show("Email invaid.");
+
+            string path = @"C:\Users\ASUS\Desktop\bibi\Nam_2\Lap_Trinh_truc_quan\SignUp_SignIn\Information_input.txt";
+            if (textBox_email.Text == "" || textBox_password.Text == "" || textBox_username.Text == "")
+            {
+                MessageBox.Show("Please fill out information.");
+            }
+            else
+            {
+                //if(File.Exists(path)==false)
+                //    File.Create(path);
+                StreamWriter test = new StreamWriter(path, true);
+                test.WriteLine(textBox_username.Text);
+                test.WriteLine(textBox_password.Text);
+                test.WriteLine(textBox_email.Text);
+                test.Close();
+
+               
+            }
+        }
+        void Auto_Fill_Out()
+        {
+            AutoCompleteStringCollection auto1 = new AutoCompleteStringCollection();
+            textBox_email.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox_email.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            string[] arrayname = { textBox_email.Text + "@gmail.com", textBox_email.Text+"@email.com" };
+            foreach (string name in arrayname)
+            {
+                auto1.Add(name);
+            }
+            textBox_email.AutoCompleteCustomSource = auto1;
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox_email_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string a = "@";
+            if (e.KeyChar == char.Parse(a))
+            {
+                Auto_Fill_Out();
+            }
+
+        }
+
+        private void textBox_email_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.G)
+            //{
+            //    AutoFillOut();
+            //}
+        }
+    }
+}
